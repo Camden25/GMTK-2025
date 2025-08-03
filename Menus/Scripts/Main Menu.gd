2b0bc6bbd9060aed8208e1d841a5960e5fc8c7ba
LiamWalkerMe <liamwalker@liamwalker.me>
2025-08-03 02:13:18 -0700

    Updated menu system with new images

--- Menus/Scripts/Main Menu.gd
+++ Menus/Scripts/Main Menu.gd
@@ -8,3 +8,21 @@ func _on_settings_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_parent().call("swap_active_menu", "quit_menu")
+	
+func _on_start_button_mouse_entered():
+	$StartButton.scale = Vector2(1.1, 1.1)
+
+func _on_start_button_mouse_exited():
+	$StartButton.scale = Vector2(1, 1)
+
+func _on_settings_button_mouse_entered():
+	$SettingsButton.scale = Vector2(1.1, 1.1)
+
+func _on_settings_button_mouse_exited():
+	$SettingsButton.scale = Vector2(1, 1)
+
+func _on_quit_button_mouse_entered():
+	$QuitButton.scale = Vector2(1.1, 1.1)
+
+func _on_quit_button_mouse_exited():
+	$QuitButton.scale = Vector2(1, 1)
